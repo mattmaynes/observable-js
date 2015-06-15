3dd62f57351e1c4f9d4e4b91a367087ad1eab922
Matthew Maynes <matthewmaynes1@gmail.com>
2015-06-14 20:58:37 -0400

    Added Unsubscribe test case

--- test/obsevable-js.spec.js
+++ test/obsevable-js.spec.js
@@ -6,7 +6,7 @@ describe('Observable-JS', function(){
 	var clock = {
 		count	: 0,
 		tick	: function(){
-			this.count++;
+			return ++this.count;
 		}
 	};
 	
@@ -18,16 +18,55 @@ describe('Observable-JS', function(){
 	});
 
 	it('Tests subscribing to an observable object', function(){
-		var count = 0;
+		var count = 0, message = '';
 		var listener = clock.subscribe('tick');
 		listener.onNext = function(tick){
 			count = tick.count;
 		};
-		
-		clock.signal(Observable.NEXT, 'tick', { count : clock.count });
-		
-		expect(count).toEqual(clock.count);
+		listener.onError = function(error){
+			message = error.message;
+		};
 
+		clock.signal(Observable.NEXT, 'tick', { count : clock.tick() });
+		clock.signal(Observable.ERROR, 'tick', { message : 'Error!' });
+		expect(count).toEqual(clock.count);
+		expect(message).toEqual('Error!');
+	});
+
+	it('Tests signaling using different streams', function(){
+		var count = 0, message = '', complete = false;
+		var	listener = clock.subscribe('tock', { 
+			onNext: function(tick){ 
+				count = tick.count; 
+			}
+		});
+		listener.onError = function(error){
+			message = error.message;
+		};
+		listener.onComplete = function(){
+			complete = true;
+		};
+
+		clock.signal(Observable.NEXT, 'tock', { count : clock.tick() });
+		clock.signal(Observable.ERROR, 'tock', { message : 'Error: Forgot how to count!' });
+		clock.signal(Observable.COMPLETE, 'tock');
+
+		expect(count).toEqual(clock.count);
+		expect(message).toEqual('Error: Forgot how to count!');
+		expect(complete).toBeTruthy();
+	});
+
+	it('Tests unsubscribing from an observer', function(){
+		var count = 0;
+		var listener = clock.subscribe('tick',{
+			onNext: function(tick){
+				count = tick.count;
+			}
+		});
+
+		clock.unsubscribe(listener);
+		clock.signal(Observable.NEXT, 'tick', { count : clock.tick() });
+		expect(count).toBe(0);
 	});
 
 });
