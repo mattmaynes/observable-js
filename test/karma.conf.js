70a9bf6063e4c76a389cceaa85c3a7340bdb30a4
Matthew Maynes <matthewmaynes1@gmail.com>
2015-06-15 08:06:32 -0400

    Added code coverage to karma tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -3,12 +3,27 @@
  */
 module.exports = function(config){
 	config.set({
-		basePath	: '../',
-		plugins		: ['karma-jasmine', 'karma-nested-reporter', 'karma-phantomjs-launcher'],
-		browsers	: ['PhantomJS'],
-		reporters	: ['progress', 'nested'],
-		frameworks	: ['jasmine'],
-		port		: 9876,
-		files		: ['src/*.js', 'test/*.spec.js']
+		// The project root path. All file paths are relative to this path
+		basePath		: '../',
+		files			: ['src/*.js', 'test/*.spec.js'],
+		plugins			: [
+			'karma-jasmine', 
+			'karma-coverage', 
+			'karma-nested-reporter', 
+			'karma-phantomjs-launcher'
+		],
+		browsers		: ['PhantomJS'],
+		reporters		: [
+			'progress', 
+			'nested', 
+			'coverage'
+		],
+		coverageReporter: {
+			type : 'text-summary'
+		},
+		frameworks		: ['jasmine'],
+		port			: 9876,
+		singleRun		: true,
+		preprocessors	: { 'src/*.js': 'coverage'}
 	});
 };
