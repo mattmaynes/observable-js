471ededce01079d98ac61a822a9403c122f1b486
Matthew Maynes <matthewmaynes1@gmail.com>
2015-06-14 20:20:35 -0400

    Added Nested Karma reporter

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,8 +4,9 @@
 module.exports = function(config){
 	config.set({
 		basePath	: '../',
-		plugins		: ['karma-jasmine', 'karma-phantomjs-launcher'],
+		plugins		: ['karma-jasmine', 'karma-nested-reporter', 'karma-phantomjs-launcher'],
 		browsers	: ['PhantomJS'],
+		reporters	: ['progress', 'nested'],
 		frameworks	: ['jasmine'],
 		port		: 9876,
 		files		: ['src/*.js', 'test/*.spec.js']
--- test/obsevable-js.spec.js
+++ test/obsevable-js.spec.js
@@ -1,7 +1,33 @@
+/**
+ * Jasmine test specification for Observable-JS
+ */ 
 describe('Observable-JS', function(){
+	
+	var clock = {
+		count	: 0,
+		tick	: function(){
+			this.count++;
+		}
+	};
+	
+	it('Tests creating an observable object', function(){
+		Observable.create(clock);
+		expect(clock.subscribe).toBeDefined();
+		expect(clock.unsubscribe).toBeDefined();
+		expect(clock.signal).toBeDefined();
+	});
 
-	it('Tests adding numbers', function(){
-		expect(1 + 1).toBe(2);
+	it('Tests subscribing to an observable object', function(){
+		var count = 0;
+		var listener = clock.subscribe('tick');
+		listener.onNext = function(tick){
+			count = tick.count;
+		};
+		
+		clock.signal(Observable.NEXT, 'tick', { count : clock.count });
+		
+		expect(count).toEqual(clock.count);
+
 	});
 
 });
